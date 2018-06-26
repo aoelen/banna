eefa46622147de419103f010db2e5b93c0688a63
Aron van Groningen <aronvgroningen@gmail.com>
2018-06-26 22:57:07 +0800

    change img

--- banna/farmer/views/overview/months.py
+++ banna/farmer/views/overview/months.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from farmer.models import Date, Farm, Report, Reports_Yield
 from django.contrib.auth.models import User
 from datetime import datetime, date
-from time import strftime
+from time import strptime
 
 
 #SHOW OVERVIEW MONTH REPORTS
@@ -20,6 +20,32 @@ def overview_months(request, farm_id):
     )
 
     reports = Report.objects.filter(farm=farm_id)
+
+    for report in reports:
+        month_name = report.month
+        month_number = strptime(month_name, '%B').tm_mon
+
+        data = {}
+        data['report'] = {
+            'farm_id': farm_id,
+            'month': report.month,
+            'year': report.year
+            }
+
+        if currentdate <= datetime(report.year, month_number, 7).date():
+            data['date'] = {
+                'value': 'test'
+            }
+        if currentdate > datetime(report.year, month_number, 7).date() and currentdate < datetime(report.year, month_number, 20).date():
+            data['date'] = {
+                'value': 'test'
+            }
+
+        if currentdate > datetime(report.year, month_number, 21).date():
+            data['date'] = {
+                'value': 'test'
+            }
+        print(data)
 
     context = {
         'reports': reports,
--- banna/farmer/views/overview/statistics.py
+++ banna/farmer/views/overview/statistics.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404, render, redirect
-
 from farmer.models import Date, Farm, Report, Reports_Yield
 from django.contrib.auth.models import User
 from datetime import datetime, date
