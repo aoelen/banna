d695189f7990e0135a1f3fdb04ebc3161f17ec2b
Aron van Groningen <aronvgroningen@gmail.com>
2018-07-02 01:23:06 +0800

    translation malay integrated

--- banna/farmer/views/login_redirect.py
+++ banna/farmer/views/login_redirect.py
@@ -1,6 +1,9 @@
 from django.shortcuts import redirect
 
 def login_redirect(request):
+    language_code = 'en'
+    request.session['language_code'] = language_code
+
     if (request.user.groups.filter(name='government').exists() or request.user.groups.filter(name='factory').exists()):
         return redirect('/dashboard/overview')
     elif (request.user.groups.filter(name='farmer').exists()):
